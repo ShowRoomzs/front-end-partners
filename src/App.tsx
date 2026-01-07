2ce74cf35a96024e1df5ce1eee6ff116853aa7fb
daeseopk <eotjqdlee@gmail.com>
2026-01-07 14:47:32 +0900

    chore: update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useRoutes } from 'react-router-dom'
-import { authRoutes, mainRoutes } from '@/common/router'
-import { useCookie } from '@/common/hooks/useCookie'
-import { COOKIE_NAME } from '@/common/constants'
+import { useRoutes } from "react-router-dom"
+import { authRoutes, mainRoutes } from "@/common/router"
+import { useCookie } from "@/common/hooks/useCookie"
+import { COOKIE_NAME } from "@/common/constants"
 
 export default function App() {
   const [accessToken] = useCookie(COOKIE_NAME.ACCESS_TOKEN)
