ff8024be7f67e0dd4cf6005324c4bf93c1dcbc7d
daeseopk <eotjqdlee@gmail.com>
2026-01-07 17:59:31 +0900

    refactor: adjust TableFooter padding and format checkbox

--- src/common/components/Table/TableFooter.tsx
+++ src/common/components/Table/TableFooter.tsx
@@ -9,7 +9,7 @@ export default function TableFooter(props: TableFooterProps) {
   const { renderLeft, renderRight } = props
 
   return (
-    <div className="sticky bottom-0 left-0 bg-[#F8F8F8] z-10 p-[15px] w-full flex flex-row items-center justify-between rounded-bl-lg rounded-br-lg">
+    <div className="sticky bottom-0 left-0 bg-[#F8F8F8] z-10  px-[15px] pt-[15px] w-full flex flex-row items-center justify-between rounded-bl-lg rounded-br-lg">
       {renderLeft ? renderLeft : <div />}
       {renderRight}
     </div>
