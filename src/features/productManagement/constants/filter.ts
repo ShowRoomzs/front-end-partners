52e3079e3c50cf474b65f3790b2a0f693302e747
daeseopk <eotjqdlee@gmail.com>
2026-01-08 15:22:07 +0900

    feat: add filter and params constants for product management

---
+++ src/features/productManagement/constants/filter.ts
@@ -0,0 +1,44 @@
+import type { FilterOptionGroup } from "@/common/components/FilterCard/FilterCard"
+import { parseMapToOptions } from "@/common/utils/parseMapToOptions"
+import {
+  PRODUCT_LIST_IS_DISPLAY_TYPE,
+  PRODUCT_LIST_IS_OUT_OF_STOCK_TYPE,
+  PRODUCT_LIST_KEYWORD_TYPE,
+} from "@/features/productManagement/constants/params"
+import type { ProductListParams } from "@/features/productManagement/services/productService"
+
+export const PRODUCT_LIST_FILTER_OPTIONS: FilterOptionGroup<ProductListParams> =
+  {
+    카테고리: [
+      {
+        type: "category",
+        key: "categoryId",
+      },
+    ],
+    "진열 여부": [
+      {
+        type: "radio",
+        key: "isDisplay",
+        options: parseMapToOptions(PRODUCT_LIST_IS_DISPLAY_TYPE),
+      },
+    ],
+    "품절 여부": [
+      {
+        type: "radio",
+        key: "isOutOfStock",
+        options: parseMapToOptions(PRODUCT_LIST_IS_OUT_OF_STOCK_TYPE),
+      },
+    ],
+    검색어: [
+      {
+        type: "select",
+        key: "keywordType",
+        options: parseMapToOptions(PRODUCT_LIST_KEYWORD_TYPE),
+      },
+      {
+        type: "input",
+        key: "keyword",
+        placeholder: "검색어를 입력하세요",
+      },
+    ],
+  }
---
+++ src/features/productManagement/constants/params.ts
@@ -0,0 +1,17 @@
+export const PRODUCT_LIST_IS_DISPLAY_TYPE = {
+  ALL: "전체",
+  true: "진열",
+  false: "미진열",
+} as const
+
+export const PRODUCT_LIST_IS_OUT_OF_STOCK_TYPE = {
+  ALL: "전체",
+  true: "품절",
+  false: "품절 아님",
+} as const
+
+export const PRODUCT_LIST_KEYWORD_TYPE = {
+  ALL: "전체",
+  name: "상품명",
+  sellerProductCode: "판매자 상품코드",
+} as const
