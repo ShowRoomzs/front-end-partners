dbb52f3999ee034af4e26639905a458723a755b4
daeseopk <eotjqdlee@gmail.com>
2026-01-09 10:11:09 +0900

    chore: add ConfirmProvider to app

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { QueryClientProvider } from '@tanstack/react-query'
-import './index.css'
-import App from './App.tsx'
-import { Toaster } from 'react-hot-toast'
-import { queryClient } from '@/common/lib/queryClient.ts'
-import { CookiesProvider } from 'react-cookie'
-import { TOAST_OPTIONS } from '@/common/constants/toast.ts'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import { QueryClientProvider } from "@tanstack/react-query"
+import "./index.css"
+import App from "./App.tsx"
+import { Toaster } from "react-hot-toast"
+import { queryClient } from "@/common/lib/queryClient.ts"
+import { CookiesProvider } from "react-cookie"
+import { TOAST_OPTIONS } from "@/common/constants/toast.ts"
+import { ConfirmProvider } from "@/common/components/ConfirmModal"
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <CookiesProvider>
         <QueryClientProvider client={queryClient}>
-          <Toaster position="top-right" toastOptions={TOAST_OPTIONS} />
-          <App />
+          <ConfirmProvider>
+            <Toaster position="top-right" toastOptions={TOAST_OPTIONS} />
+            <App />
+          </ConfirmProvider>
         </QueryClientProvider>
       </CookiesProvider>
     </BrowserRouter>
