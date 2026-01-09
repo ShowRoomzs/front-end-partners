93f9820cb6d05d4a0db8c748f9224e9cb870e445
daeseopk <eotjqdlee@gmail.com>
2026-01-09 10:04:12 +0900

    feat: support all option in parseMapToOptions

--- src/common/utils/parseMapToOptions.ts
+++ src/common/utils/parseMapToOptions.ts
@@ -1,6 +1,21 @@
-export function parseMapToOptions<T extends Record<string, string>>(map: T) {
-  return Object.entries(map).map(([key, value]) => ({
-    label: value,
-    value: key,
-  }))
+import type { Option } from "@/common/types/option"
+
+export function parseMapToOptions<T extends Record<string, string>>(
+  map: T,
+  hasAllOption: boolean = false
+) {
+  const options: Array<Option<string | null>> = Object.entries(map).map(
+    ([key, value]) => ({
+      label: value,
+      value: key,
+    })
+  )
+
+  if (hasAllOption) {
+    options.unshift({
+      label: "전체",
+      value: null,
+    })
+  }
+  return options
 }
